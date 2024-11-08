16439afb55fb75ea5a6be06d746c1de0a55ca41f
Abhay Raj Singh <abhayonlyone@gmail.com>
2024-11-09 05:04:15 +0530

    feat: ts-rest fastify example

---
+++ examples/fastify/main.ts
@@ -0,0 +1,36 @@
+import Fastify from 'fastify'
+import { initServer } from '@ts-rest/fastify'
+import { contract } from './contract'
+import { TsRestResponse } from '../../dist/ts-rest'
+
+const app = Fastify()
+
+const s = initServer()
+
+const router = s.router(contract, {
+	getPost: async ({ params: { id } }) => {
+		const post = { article: `Hello There ${id}` }
+
+		return TsRestResponse.Ok(post)
+	},
+	createPost: async ({ body }) => {
+		const post = { article: 'Hello There' }
+
+		console.log(body)
+
+		return TsRestResponse.Created(post)
+	},
+})
+
+app.register(s.plugin(router))
+
+const start = async () => {
+	try {
+		await app.listen({ port: 3000 })
+	} catch (err) {
+		app.log.error(err)
+		process.exit(1)
+	}
+}
+
+start()
